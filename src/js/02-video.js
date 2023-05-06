4613fa36eb9ca7de3dad188a7f283a32f6d878e8
Serhii <38428786+SergioUkraine@users.noreply.github.com>
2023-05-06 22:37:47 +0300

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+const throttle = require('lodash.throttle');
+
+const onPlay = function(event) {
+    localStorage.setItem("videoplayer-current-time", event.seconds);
+    console.log(event.seconds);
+    console.log(localStorage.getItem("videoplayer-current-time"));
+};
+
+const throttledOnPlay = throttle(onPlay, 1000);
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+player.on('timeupdate', throttledOnPlay);
